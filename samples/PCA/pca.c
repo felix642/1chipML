32fdee34f4821cfa54f9e2f14b85c35e0afd7562
Felix <felix-antoine.constantin@polymtl.ca>
2023-03-04 23:09:14 -0500

    PCA seems to be working now

--- samples/PCA/pca.c
+++ samples/PCA/pca.c
@@ -14,7 +14,7 @@
 #include <lanczos.h>
 #include <jacobi.h>
 
-int loadSample(char* sampleName, uint8_t nbDataPoints, uint8_t nbDims, uint8_t* outputArray)
+int loadSample(char* sampleName, uint8_t nbDataPoints, uint8_t nbDims, float* outputArray)
 {
   FILE* file = fopen(sampleName, "r");
   if(file == NULL)
@@ -37,7 +37,7 @@ int loadSample(char* sampleName, uint8_t nbDataPoints, uint8_t nbDims, uint8_t* 
       double value = atof(ptr);
       ptr = strtok(NULL, "\t");
       // We store values in uint8_t to save space
-      outputArray[currentInx++] = value * 10;
+      outputArray[currentInx++] = value;
     }
   }
 
@@ -57,7 +57,7 @@ void printMatrix(float* ptr, int row, int column)
   }
 }
 
-float findMean(uint8_t* ptr, uint8_t nbValues, uint8_t offset, uint8_t stride)
+float findMean(float* ptr, uint8_t nbValues, uint8_t offset, uint8_t stride)
 {
   double mean = 0;
   for(int i = 0; i < nbValues; i++)
@@ -68,10 +68,10 @@ float findMean(uint8_t* ptr, uint8_t nbValues, uint8_t offset, uint8_t stride)
 }
 
 
-void computeCovarianceMatrix(uint8_t* ptr, uint8_t nbElemsCov, uint8_t nbElems, float* output)
+void computeCovarianceMatrix(float* ptr, uint8_t nbElemsCov, uint8_t nbElems, float* output)
 {
   float means[nbElemsCov];
-  const uint8_t stride = 5;
+  const uint8_t stride = 4;
   for(int i = 0; i < nbElemsCov; ++i)
   {
     means[i] = findMean(ptr, nbElems, i, stride);
@@ -92,23 +92,61 @@ void computeCovarianceMatrix(uint8_t* ptr, uint8_t nbElemsCov, uint8_t nbElems, 
   }
 }
 
+void writeDataCsv(float* data, int nbRows, int nbColumns, char* fileName)
+{
+  FILE* file = fopen(fileName, "w");
+  for(int i = 0; i < nbRows; ++i)
+  {
+      fprintf(file, "%f, %f\n", data[i * nbColumns], data[i * nbColumns + 1]);
+  }
+
+  fclose(file);
+}
+
 
 int main()
 {
   float covarianceMatrix[4 * 4];
-  uint8_t sample[150 * 5];
-  loadSample("irisdata.txt", 150, 5, sample);
-  computeCovarianceMatrix(sample, 4, 150, covarianceMatrix); 
+  float sample[100 * 4];
+  // load sample from text file and compute covariance Matrix
+  loadSample("irisdata.txt", 100, 4, sample);
+  computeCovarianceMatrix(sample, 4, 100, covarianceMatrix); 
 
   float tMatrix[4 * 4];
   memset(tMatrix, 0, 4 * 4 * sizeof(float));
   float vMatrix[4 * 4];
+  // Apply lanczos for tridiagonalization
   lanczos(covarianceMatrix, 4, 4, NULL, tMatrix, vMatrix);
 
   float eigenVectors[4 * 4];
-  jacobi(tMatrix, 4, eigenVectors, 4, 0);
+  // Apply jacobi to find eigenvalues and eigenvectors of matrix T
+  jacobi(tMatrix, 4, eigenVectors, 25, 1);
+  float realEigenVectors[4 * 4];
+  uint_least8_t size[] = {4, 4, 4};
+  // Transform eigenvectors from Matrix T to covariance Matrix using matrix V
+  matrixMultiply(vMatrix, eigenVectors, size, realEigenVectors);
   
-  printMatrix(tMatrix, 4, 4);
+  float transposeRealVectors[4 * 4];
+  // Transpose matrix to extract eigenvectors more easily
+  transpose(realEigenVectors, transposeRealVectors, size);
 
+  float w_matrix[2 * 4];
+  memcpy(w_matrix, transposeRealVectors, 4 * sizeof(float));
+  memcpy(&w_matrix[4], &transposeRealVectors[4 * 2], 4 * sizeof(float));
+
+  float wTranspose[2 * 4];
+  uint_least8_t size_2[] = {2, 4};
+  transpose(w_matrix, wTranspose, size_2);
+
+
+  float outputPCA[100 * 2];
+  uint_least8_t size_final[] = {100, 4, 2};
+  matrixMultiply(sample, w_matrix, size_final, outputPCA);
+
+  printMatrix(outputPCA, 100, 2);
+
+  writeDataCsv(outputPCA, 100, 2, "output.csv");
+  
+  
   return 0;
 }
